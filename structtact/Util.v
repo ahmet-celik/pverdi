7aea200ca9a7c038dada9518abb5a678c795108e
Ahmet Celik <ahmetcelik@utexas.edu>
2016-10-12 23:14:21 -0500

    2cb92f5561db5d2058c8e61c11f442ced9561343

--- structtact/Util.v
+++ structtact/Util.v
@@ -1008,7 +1008,7 @@ Section assoc.
       | [] => []
       | (k', v') :: l' =>
         if K_eq_dec k k' then
-          l'
+          assoc_del l' k
         else
           (k', v') :: (assoc_del l' k)
     end.
@@ -1050,15 +1050,12 @@ Section assoc.
 
   Lemma get_del_same :
     forall k l,
-      NoDup (map (@fst _ _) l) ->
       assoc (assoc_del l k) k = None.
   Proof using. 
     induction l; intros; simpl in *.
     - auto.
-    - invc_NoDup.
-      repeat break_match; subst; simpl in *.
-      + apply not_in_assoc. auto.
-      + break_if; try congruence. auto.
+    - repeat break_match; subst; simpl in *; auto.
+      break_if; try congruence.
   Qed.
 
   Lemma get_del_diff :
@@ -1068,8 +1065,7 @@ Section assoc.
   Proof using. 
     induction l; intros; simpl in *.
     - auto.
-    - repeat (break_match; simpl); subst; try congruence.
-      auto.
+    - repeat (break_match; simpl); subst; try congruence; auto.
   Qed.
 
   Lemma get_set_diff_default :
